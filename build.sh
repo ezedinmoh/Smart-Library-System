7dd283fca489174885d155ef0b8d76bab663d7c5
ezedinmoh <ezedinmoh1@gmail.com>
2026-05-22 00:33:31 +0300

    Fix some issues

--- build.sh
+++ build.sh
@@ -8,3 +8,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input --settings=library_system.settings_production
 
 python manage.py migrate --settings=library_system.settings_production
+
+python manage.py generate_qr_codes --settings=library_system.settings_production
